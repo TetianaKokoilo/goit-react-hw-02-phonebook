21c77cafe3b396fd52fb1a77cc34af0c9576cb13
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-23 14:35:47 +0200

    add id for number

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,16 +1,20 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 
 export class ContactForm extends Component {
   state = {
     name: '',
+    number: '',
   };
 
   hanldeNameChange = e => {
-    this.setState({name: e.target.value});
-  }
+    this.setState({ name: e.target.value });
+  };
 
-    loginInputName = nanoid();
+  loginInputName = nanoid();
+  loginInputnumber = nanoid();
+
+
   render() {
     return (
       <form>
@@ -22,7 +26,16 @@ export class ContactForm extends Component {
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           id={this.loginInputName}
-          onChange = {this.hanldeNameChange}
+          onChange={this.hanldeNameChange}
+        />
+        <h2>Number</h2>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          id={this.loginInputnumber}
         />
         <button type="submit">Add Contacts</button>
       </form>
