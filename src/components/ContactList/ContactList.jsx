b6afa86093291a64aae4e907f74a3d04d9654c38
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-23 16:50:41 +0200

    add ContactList render

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,16 @@
+
+export const ContactList = ({ contacts}) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          <p>{name}</p>
+          <p>{number}</p>
+          <button type="button">
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
