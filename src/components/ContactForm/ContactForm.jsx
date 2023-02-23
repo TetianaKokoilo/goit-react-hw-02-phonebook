3993b7329743d6f99b2bf3a3bb0cd68597991ae5
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-23 14:50:18 +0200

    add handleChange for ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -7,9 +7,21 @@ export class ContactForm extends Component {
     number: '',
   };
 
+  handleChange = e => {
+    // console.log(e.target);
+    // console.log(e.target.name);
+    // console.log(e.target.value);
+    const {name, value} = e.target;
+    this.setState({[name]: value});
+  }
+
   hanldeNameChange = e => {
     this.setState({ name: e.target.value });
   };
+
+  hanldeNumberChange = e => {
+    this.setState({number: e.target.value})
+  }
 
   loginInputName = nanoid();
   loginInputnumber = nanoid();
@@ -26,7 +38,7 @@ export class ContactForm extends Component {
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           id={this.loginInputName}
-          onChange={this.hanldeNameChange}
+          onChange={this.handleChange}
         />
         <h2>Number</h2>
         <input
@@ -36,6 +48,7 @@ export class ContactForm extends Component {
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           id={this.loginInputnumber}
+          onChange={this.handleChange}
         />
         <button type="submit">Add Contacts</button>
       </form>
